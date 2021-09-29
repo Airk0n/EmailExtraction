e6ca2ca48a713870c9a4d8beebd01a7812991a01
Aaron Spencer <aaronspencer@softwire.com>
2021-09-29 10:14:19 +0100

    Starting point

---
+++ Email extraction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Email_extraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Create our list of accepted domains
+             // Create a Regex  
+            string pattern = @"\b[M]\w+";
+            String testString = "hey";
+            Regex rg = new Regex(pattern);
+            Match match = rg.Match(testString);
+            
+             */
+            
+            
+            //Reading from a textfile
+            // string rawFile = File.ReadAllText("../../../sample.txt");  
+            // Console.WriteLine(text);  
+        }
+    }
+}
