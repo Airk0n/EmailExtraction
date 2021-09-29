072ff5b82bd9ca55a067dd263e948c2e872325ae
Aaron Spencer <aaronspencer@softwire.com>
2021-09-29 13:55:39 +0100

    Using a dictionary

--- Email extraction/Program.cs
+++ Email extraction/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Email_extraction
@@ -8,21 +10,48 @@ namespace Email_extraction
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> toSort = CountDomains().OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var domain in toSort)
+            {
+                Console.WriteLine("key: " + domain.Key.ToString() + " value: "+ domain.Value);
+            }
+
+        }
+        
+        
+        
+        
+        private static Dictionary<string, int> CountDomains()
+        {
+            /*
+ Regex for email addresses
+ Check domain
+ If it exsists in dictionary add to counter
+ sort by highest counter and print
+ */
+            Dictionary<String, int> dict = new Dictionary<string, int>();
             string rawFile = File.ReadAllText("../../../sample.txt");
-            string pattern = @"\S+@softwire.com";
-            string pattern2 = @"\w+";
-            Regex regex = new Regex(pattern);
+            string wholeEmailPattern = @"(\w+[._-]?\w*){1,2}@(\w+).\w{2,3}([.]\w{2,3})?[ |\n]";
+            Regex regex = new Regex(wholeEmailPattern);
             MatchCollection matchCollection = regex.Matches(rawFile);
-            int counter = 0;
-            Console.WriteLine(matchCollection.Count.ToString());
+            Console.WriteLine(matchCollection.First().Groups[2].Value);
+            
             foreach (Match match in matchCollection)
             {
-                Console.WriteLine("match contents: " + match.Value);
-                counter++;
+                string domain = match.Groups[2].Value;
+                
+                if (dict.ContainsKey(domain))
+                {
+                    dict[domain]++;
+                    continue;
+                }
+                dict.Add(domain,1);
             }
-           
 
-            Console.WriteLine($"There are {counter} emails that contain '@softwire.com'");
+            return dict;
         }
+        
+
+
     }
 }
